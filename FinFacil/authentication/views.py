51559fcb50356c5bc9181f426f7016089143d68c
LucasTaveira <lucastaveira.lt@gmail.com>
2025-01-08 19:28:22 -0300

    remove docstring

--- FinFacil/authentication/views.py
+++ FinFacil/authentication/views.py
@@ -9,18 +9,7 @@ class AuthenticationUserView(
     mixins.CreateModelMixin, 
     generics.GenericAPIView
 ):
-    """
-    ### Description:
-        This model should be used to create a authenticator to users
-        
-    ### Fields:
-        - email: The email of the user
-        - password: The password of the user
-        
-    ### Methods:
-        - POST
-    """
-    
+   
     queryset = AuthenticationUser.objects.all()
     serializer_class = AuthenticationUserSerializer
     permission_classes = [AllowAny]
@@ -29,23 +18,6 @@ class AuthenticationUserView(
         return super().create(request, *args, **kwargs)
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    ### Description:
-        This model should be used to create a user
-        
-    ### Fields:
-        - first_name: The first name of the user
-        - last_name: The last name of the user (optional)
-        - whatsApp: phone number (optional)
-        - user_type: USER or API (API is for who whant to use this API)
-        
-    ### Methods:
-        - POST
-        - GET
-        - PUT
-        - DELETE
-    """
-    
     serializer_class = UserSerializer
     
     def get_permissions(self):
