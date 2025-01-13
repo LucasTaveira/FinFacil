6eeaafb1a5da26b873ecf329947a09d2fe085519
LucasTaveira <lucastaveira.lt@gmail.com>
2025-01-13 18:38:41 -0300

    create models, serializer and views to histories taks

---
+++ FinFacil/control/models/history.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from .incomes import UserIncomeItens
+from .monthly_expense import MonthlyExpenseItens
+from .spending_plan import SpendingPlanItens
+
+class History(models.Model):
+    update = models.JSONField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class UserIncomeItensHistory(models.Model):
+    user_income_itens = models.ForeignKey(UserIncomeItens, on_delete=models.CASCADE)
+    History = models.ForeignKey(History, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class SpendingPlanItensHistory(models.Model):
+    spending_plan_itens = models.ForeignKey(SpendingPlanItens, on_delete=models.CASCADE)
+    History = models.ForeignKey(History, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class MonthlyExpenseItensHistory(models.Model):
+    monthly_expense_itens = models.ForeignKey(MonthlyExpenseItens, on_delete=models.CASCADE)
+    History = models.ForeignKey(History, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ FinFacil/control/views/history_view.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+
+from ..models.history import (
+    History, 
+    UserIncomeItensHistory, 
+    MonthlyExpenseItensHistory, 
+    SpendingPlanItensHistory
+)
+from ..serializers.history_serializer import (
+    HistorySerializer,
+    UserIncomeItensHistorySerializer,
+    MonthlyExpenseItensHistorySerializer,
+    SpendingPlanItensHistorySerializer
+)
+
+class HistoryView(viewsets.ModelViewSet):
+    serializer_class = HistorySerializer
+    
+    def get_queryset(self):
+        try:
+            if self.request.user.is_authenticated and self.request.user.is_staff:
+                return History.objects.all()
+        except History.DoesNotExist:
+            return super().get_queryset()
+    
+class UserIncomeItensHistoryView(viewsets.ReadOnlyModelViewSet):
+    queryset = UserIncomeItensHistory.objects.all()
+    serializer_class = UserIncomeItensHistorySerializer
+    
+class MonthlyExpenseItensHistoryView(viewsets.ReadOnlyModelViewSet):
+    queryset = MonthlyExpenseItensHistory.objects.all()
+    serializer_class = MonthlyExpenseItensHistorySerializer 
+    
+class SpendingPlanItensHistoryView(viewsets.ReadOnlyModelViewSet):
+    queryset = SpendingPlanItensHistory.objects.all()
+    serializer_class = SpendingPlanItensHistorySerializer
